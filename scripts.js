743a28d45bc834029f66f1b00f12b8538db44cdd
Ima1H <71931682+Ima1H@users.noreply.github.com>
2025-10-18 00:52:44 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -120,7 +120,7 @@ $(document).ready(function() {
 
 
   lightGallery(document.getElementById('lightgallery'), {
-    selector: 'a', 
+    selector: '.lightgallery-img', 
     speed: 500,
     download: false
   });
