4c9c7dccda8e7a99fecbb93450bba951839cc4ee
Ima1H <71931682+Ima1H@users.noreply.github.com>
2025-12-10 18:42:39 +0100

    update

--- scripts.js
+++ scripts.js
@@ -130,11 +130,34 @@ $(document).ready(function() {
     slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [
+     {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
         }
       }
     ]
